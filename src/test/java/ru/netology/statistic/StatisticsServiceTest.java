7a890a70362b4aa751bad841334fa80f672c2090
Victoria Pavlova <pvd0510@mail.ru>
2022-01-25 19:35:28 +0300

    Code Coverage 100

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,6 +10,18 @@ class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findMaxNewDate() {
+        StatisticsService service = new StatisticsService();
+
         long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
